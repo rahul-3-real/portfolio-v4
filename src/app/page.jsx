43c30beaf2b370beb5b91e990c3f2254d7882c68
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-10-24 16:53:26 +0530

    Added Bio Section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,16 @@
+import Bio from "@/components/sections/Bio";
 import Hero from "@/components/sections/Hero";
 
 const page = () => {
   return (
     <div className="container">
-      <div className="grid sm:grid-cols-2 grid-cols-1 gap-10">
+      <div className="grid sm:grid-cols-2 grid-cols-1 gap-10 items-start">
         <div className="col">
           <Hero />
         </div>
-        <div className="col"></div>
+        <div className="col">
+          <Bio />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/sections/Bio.jsx
@@ -0,0 +1,38 @@
+const Bio = () => {
+  return (
+    <div className="bio-section">
+      <p className="mb-5">
+        Back in 2019, I started my journey in web development by creating my
+        first website, <b className="text-light">MeeN'Music</b>, where users
+        could listen to, download, and read song lyrics. Though it faced
+        copyright issues, it sparked my passion for programming and design. I
+        did courses and pursued my interest in{" "}
+        <b className="text-light">programming</b>,{" "}
+        <b className="text-light">freelancing</b>, and working on{" "}
+        <b className="text-light">personal projects</b>. which allowed me to
+        develop a wide range of skills across various web technologies.
+      </p>
+      <p className="mb-5">
+        Over the years, I've built websites for{" "}
+        <b className="text-light">local businesses</b> and worked at{" "}
+        <b className="text-light">various companies</b>, gaining practical
+        experience in real-world projects that challenged me to continually
+        grow. I enjoy the{" "}
+        <b className="text-light">blend of design and development</b>, striving
+        to create beautiful, functional, and optimized websites. While I prefer
+        working solo, collaborating with like-minded people has taught me the
+        value of teamwork and shared creativity.
+      </p>
+      <p>
+        As a <b className="text-light">self-taught developer</b>, I'm always
+        exploring new technologies and pushing myself to stay ahead of the curve
+        in the rapidly evolving world of development. What drives me is the
+        pursuit of creating <b className="text-light">smooth</b>,{" "}
+        <b className="text-light">engaging experiences</b> that not only look
+        great but are also intuitive and highly functional for users.
+      </p>
+    </div>
+  );
+};
+
+export default Bio;
